4b1fe1c8b718b05688b701741c23358d9bcb8c74
n00b87 <n00bskillz87@gmail.com>
2017-12-10 20:26:54 -0600

    Delete rc_global.h

--- rc_global.h
+++
@@ -1,9 +0,0 @@
-#ifndef RC_GLOBAL_INCLUDED
-#define RC_GLOBAL_INCLUDED
-
-vector<string> vm_asm; //the generated opcodes for the current set of
-vector<string> vm_data_asm;
-
-int rid_count = 0;
-
-#endif // RC_GLOBAL_INCLUDED
