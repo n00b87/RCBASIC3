57eb0c96e47f2bf02cc8e6fa7b1a8879f129f0d2
n00b87 <n00bskillz87@gmail.com>
2020-04-29 00:57:24 -0500

    Consolidated all OS defines into one file
    All OS defines are set from this one file now

---
+++ rcbasic_runtime/rc_os_defines.h
@@ -0,0 +1,15 @@
+#ifndef RC_OS_DEFINES_H_INCLUDED
+#define RC_OS_DEFINES_H_INCLUDED
+
+#define RC_LINUX
+#define RC_WEB
+//#define RC_WINDOWS
+//#define RC_ANDROID
+//#define RC_MAC
+//#define RC_IOS
+
+#ifdef RC_WEB
+#define RC_LINUX
+#endif
+
+#endif // RC_OS_DEFINES_H_INCLUDED
