3467d1849721958e5fd9857649c40d782fc90f34
n00b87 <n00bskillz87@gmail.com>
2020-12-03 00:45:42 -0600

    Updated Android.mk

--- rcbasic_runtime/Android.mk
+++ rcbasic_runtime/Android.mk
@@ -5,15 +5,16 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := main
 
 SDL_PATH := ../SDL
+#VORBIS_LIBRARY_PATH := ../libvorbisidec-1.2.1
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include $(LOCAL_PATH)/../SDL2_image/include $(LOCAL_PATH)/../SDL2_ttf/include $(LOCAL_PATH)/../SDL2_mixer/include $(LOCAL_PATH)/../SDL2_net/include $(LOCAL_PATH)/../SDL2_gfx $(LOCAL_PATH)/../theora $(LOCAL_PATH)/../libvorbis-1.3.3/include
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include $(LOCAL_PATH)/../SDL2_image/include $(LOCAL_PATH)/../SDL2_ttf/include $(LOCAL_PATH)/../SDL2_mixer/include $(LOCAL_PATH)/../SDL2_net/include $(LOCAL_PATH)/../SDL2_gfx $(LOCAL_PATH)/../theora $(LOCAL_PATH)/../libvorbisidec-1.2.1
 
 # Add your application source files here...
 LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.c \
 	main.cpp \
 	theoraplay.c
 
-LOCAL_SHARED_LIBRARIES := SDL2 SDL2_image SDL2_ttf SDL2_mixer SDL2_net SDL2_gfx theora
+LOCAL_SHARED_LIBRARIES := SDL2 SDL2_image SDL2_ttf SDL2_mixer SDL2_net SDL2_gfx ogg vorbis theora
 
 LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -llog
 
