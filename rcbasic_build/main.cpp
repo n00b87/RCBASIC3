9762865d228873e95cfc576ec3f9a4c6f442a053
n00b87 <n00bskillz87@gmail.com>
2022-02-12 22:57:58 -0600

    Fixed multiple argument parsing bug
    There was an issue parsing multiple arguments that would allow a technically incorrect expression to be evaluated as a success

--- rcbasic_build/main.cpp
+++ rcbasic_build/main.cpp
@@ -750,7 +750,7 @@ int main(int argc, char * argv[])
 
     if(rc_filename.compare("--version")==0)
     {
-        cout << "RCBASIC Compiler v3.17" << endl;
+        cout << "RCBASIC Compiler v3.18" << endl;
         return 0;
     }
 
